3c73ea0c215196a1732cb35487344bc9f36c7e75
chisacam <dladsds123@gmail.com>
2022-11-24 16:31:54 +0900

    feat: move container to center

--- contentscript-desktop.js
+++ contentscript-desktop.js
@@ -1,6 +1,8 @@
-const targetBody = document.querySelectorAll('#NM_FAVORITE > div.group_nav , #header > div > div > div.service_area , #NM_INT_LEFT , #shopcast , #gnb , #footer , #timesquare')
+const targetBody = document.querySelectorAll('#NM_FAVORITE > div.group_nav , #header > div > div > div.service_area , #NM_INT_LEFT , #shopcast , #gnb , #footer , #timesquare , #container > div:nth-child(1) , #NM_scroll_top_btn , #NM_darkmode_btn');
 for(const target of targetBody) {
     target.remove()
-}
-let wrap = document.querySelector('#wrap')
-wrap.setAttribute('style', 'overflow: inherit')
+};
+let wrap = document.querySelector('#wrap');
+wrap.setAttribute('style', 'overflow: inherit');
+let container = document.querySelector('#container');
+container.setAttribute('style', 'position: fixed;');
