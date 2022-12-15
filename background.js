52026780af625656abc5047b49834ed1d4577bf4
chisacam <37512491+chisacam@users.noreply.github.com>
2022-12-15 16:34:30 +0900

    remove console.log

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log(tabId, changeInfo, tab)
     if (changeInfo.status === 'complete' && /www.naver.com/.test(tab.url)) {
         chrome.scripting.executeScript({
             target: { tabId: tabId },
